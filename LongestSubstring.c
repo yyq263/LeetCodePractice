1aff484579fb70b489526e4fc0702966011d0823
Yiqin Yao <yy1165@nyu.edu>
2016-10-13 23:26:58 +0800

    TODO: lengthOfLongestSubstring

--- LongestSubstring.c
+++ LongestSubstring.c
@@ -4,4 +4,26 @@
 
 int lengthOfLongestSubstring(char* s) {
 
+	int i = 0;
+	int length = strlen(s);
+	int s_hash[26] = {0};
+	int beg = 0, end = 0;
+	int ans = 1;
+
+
+	while (beg < length && end < length)
+	{
+		if (s_hash[s[end] - 'a'] <= 1)
+		{
+			end++;
+			s_hash[s[end] - 'a'] += 1;
+			ans = ans > (end - beg + 1) ? ans : (end - beg + 1);
+		}
+		else
+		{
+			s_hash[s[end] - 'a'] = 0;
+			beg++;
+		}
+	}
+	return ans;
 }
